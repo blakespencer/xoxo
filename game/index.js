4d8353aac4e1e49724dfe91938b35bf17a619424
Blake Spencer <blake.spencer1@gmail.com>
2018-05-16 15:21:50 -0400

    extra credit here we cooooomeeeeee

--- game/index.js
+++ game/index.js
@@ -3,9 +3,9 @@ import { combineReducers } from 'redux';
 // const board = Map()
 
 function streak(board, first, second, third) {
-  let player0
-  let player1
-  let player2
+  let player0;
+  let player1;
+  let player2;
   if (board.hasIn(first)) {
     player0 = board.getIn(first);
   }
@@ -26,22 +26,22 @@ function winner(board) {
   for (let i = 0; i < 3; i++) {
     let result = streak(board, [i, 0], [i, 1], [i, 2]);
     if (result) {
-      return result + " wins!";
+      return result + ' wins!';
     }
   }
   for (let i = 0; i < 3; i++) {
     let result = streak(board, [0, i], [1, i], [2, i]);
     if (result) {
-      return result + " wins!";;
+      return result + ' wins!';
     }
   }
   let result = streak(board, [0, 0], [1, 1], [2, 2]);
   if (result) {
-    return result + " wins!";
+    return result + ' wins!';
   }
   result = streak(board, [0, 2], [1, 1], [2, 0]);
   if (result) {
-    return result + " wins!";
+    return result + ' wins!';
   }
 
   for (let i = 0; i < 3; i++) {
@@ -56,6 +56,7 @@ function winner(board) {
 }
 
 export const move = (player, arr) => {
+  console.log('inside move arr ', arr);
   return {
     type: 'MOVE',
     position: arr,
@@ -63,52 +64,65 @@ export const move = (player, arr) => {
   };
 };
 
-
+function bad(state, action) {
+  console.log('this is action ', action.position);
+  const coord = action.position;
+  if (
+    !(
+      Array.isArray(coord) &&
+      coord[0] < 3 &&
+      coord[0] > -1 &&
+      coord[1] > -1 &&
+      coord[1] < 3
+    )
+  ) {
+    return 'Naughty make valid coordinates!';
+  }
+  if (state.board.hasIn(action.position)) {
+    return 'No cheating pick your own spot';
+  }
+  return null;
+}
 
 const turnReducer = (turn = 'X', action) => {
   switch (action.type) {
-    case "MOVE":
+    case 'MOVE':
       turn = turn === 'X' ? 'O' : 'X';
-      return turn
+      return turn;
     default:
-      return turn
+      return turn;
   }
-}
-
+};
 
 const boardReducer = (board = Map(), action) => {
   switch (action.type) {
-    case "MOVE":
+    case 'MOVE':
       board = board.setIn(action.position, action.player);
-      return board
+      return board;
     default:
-      return board
+      return board;
   }
-}
-
+};
 
 function reducer(state = {}, action) {
-  const winBoard = boardReducer(state.board, action)
-  const win = winner(winBoard)
+  if (action.type === 'MOVE') {
+    const error = bad(state, action);
+    if (error !== null) {
+      return {
+        board: state.board,
+        turn: state.turn,
+        win: state.win,
+        error,
+      };
+    }
+  }
+  const winBoard = boardReducer(state.board, action);
+  const win = winner(winBoard);
   return {
     board: boardReducer(state.board, action),
     turn: turnReducer(state.turn, action),
-    win
-  }
+    win,
+  };
 }
 
-export default reducer
-
-
-// export default function reducer(state = { turn: 'X', board }, action) {
-//   // TODO
-//   switch (action.type) {
-//     case 'MOVE':
-//       const board = state.board.setIn(action.position, action.player);
-//       const turn = state.turn === 'X' ? 'O' : 'X';
-//       return { turn, board };
-//     default:
-//       return state;
-//   }
-// }
-
+export default reducer;
